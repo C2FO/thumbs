b44a89e527f976acb59d5d65dcd525c91ac3c110
Doug Martin <doug@dougamartin.com>
2012-11-10 11:39:30 -0600

    added the ability to have multiple events on an element

--- test/spec/delegateSpec.js
+++ test/spec/delegateSpec.js
@@ -1,33 +1,71 @@
 describe("thumbs.View event", function () {
-    var spy = sinon.spy(), View = thumbs.View.extend({
-        template: '<button data-thumbs-event="click:testFunc">Test Button</button>',
-        render: function () {
-            this.$el.html(this.template);
-            return this._super('render', arguments);
-        },
-        testObj: 'hello',
-        testFunc: spy
-    });
-    var view = null;
+    describe("single events", function () {
+        var spy = sinon.spy(), View = thumbs.View.extend({
+            template: '<button data-thumbs-event="click:testFunc">Test Button</button>',
+            render: function () {
+                this.$el.html(this.template);
+                return this._super('render', arguments);
+            },
+            testObj: 'hello',
+            testFunc: spy
+        });
+        var view = null;
 
-    beforeEach(function () {
-        view = new View();
+        beforeEach(function () {
+            view = new View();
+        });
+
+        afterEach(function () {
+            view = null;
+            spy.reset();
+        });
+
+        it("adds events set in the template to the events object", function () {
+            expect(view.events).not.toBeDefined();
+            view.render();
+            expect(view.events).toBeDefined();
+        });
+
+        it("allows events to be set directly on an element", function () {
+            view.render();
+            view.$('button').trigger('click');
+            expect(spy).toHaveBeenCalledOnce();
+        });
     });
 
-    afterEach(function () {
-        view = null;
-        spy.reset();
-    });
+    describe("multiple events", function () {
+        var spy = sinon.spy(), View = thumbs.View.extend({
+            template: '<button data-thumbs-event="click:testFunc focus:testFunc">Test Button</button>',
+            render: function () {
+                this.$el.html(this.template);
+                return this._super('render', arguments);
+            },
+            testObj: 'hello',
+            testFunc: spy
+        });
+        var view = null;
 
-    it("adds events set in the template to the events object", function () {
-        expect(view.events).not.toBeDefined();
-        view.render();
-        expect(view.events).toBeDefined();
-    });
+        beforeEach(function () {
+            view = new View();
+        });
 
-    it("allows events to be set directly on an element", function () {
-        view.render();
-        view.$('button').trigger('click');
-        expect(spy).toHaveBeenCalledOnce();
+        afterEach(function () {
+            view = null;
+            spy.reset();
+        });
+
+        it("adds events set in the template to the events object", function () {
+            expect(view.events).not.toBeDefined();
+            view.render();
+            expect(view.events).toBeDefined();
+        });
+
+        it("allows events to be set directly on an element", function () {
+            view.render();
+            view.$('button').trigger('click');
+            expect(spy).toHaveBeenCalledOnce();
+            view.$('button').trigger('focus');
+            expect(spy).toHaveBeenCalledTwice();
+        });
     });
 });
