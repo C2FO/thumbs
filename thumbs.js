367e008eaad2076f0658c57cadda86f6257a5519
Doug Martin <doug@dougamartin.com>
2012-11-09 19:42:21 -0600

    changed to use Original Backbone extend

--- thumbs.js
+++ thumbs.js
@@ -65,47 +65,16 @@
         }())
     };
 
+
+    // this allows access to prototype.constructor.__super__
+    // when _.bindAll is used
+    var _extend = Thumbs.Model.extend;
+
     function extend(protoProps, staticProps) {
-        /*jshint validthis:true camelcase:false*/
-        var parent = this;
-        var child, childProto;
-
-        // The constructor function for the new subclass is either defined by you
-        // (the "constructor" property in your `extend` definition), or defaulted
-        // by us to simply call the parent's constructor.
-        if (protoProps && _.has(protoProps, 'constructor')) {
-            child = protoProps.constructor;
-        } else {
-            child = function () {
-                parent.apply(this, arguments);
-            };
-        }
-        // Set the prototype chain to inherit from `parent`, without calling
-        // `parent`'s constructor function.
-        function Surrogate() {
-            this.constructor = child;
-        }
-
-        Surrogate.prototype = parent.prototype;
-        childProto = child.prototype = new Surrogate();
-
-        // Add prototype properties (instance properties) to the subclass,
-        // if supplied.
-        if (protoProps) {
-            _.extend(child.prototype, protoProps);
-        }
-
-        // Add static properties to the constructor function, if supplied.
-        _.extend(child, parent, staticProps);
-
-        // Set a convenience property in case the parent's prototype is needed
-        // later.
-        child.__super__ = parent.prototype;
-
-        childProto.__getConstructor = function __getConstructor() {
+        var child = _extend.apply(this, arguments);
+        child.prototype.__getConstructor = function () {
             return child;
-        }
-
+        };
         return child;
     }
 
@@ -163,11 +132,6 @@
 
     View = Thumbs.View = View.extend(EventMonitor).extend({
         _subviews: null,
-
-        // child : null,
-        // _childInstance : null,
-        // channel : '',
-        locals: null, // properties to add to the template for rendering
 
         initialize: function initialize(options) {
             this._super("render", arguments);
